11208c28489d0a6c51420949e0a2e01232f20b4e
Eduardo Zavarce <eduzavarce@gmail.com>
2025-07-25 23:02:04 +0200

    shared context initial commit

---
+++ src/shared/src/test/java/dev/eduzavarce/shared/SharedApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eduzavarce.shared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
